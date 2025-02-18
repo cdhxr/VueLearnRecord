648a8169bec5ce0f99e89a9897ff73c1735ed4cb
Cdihe <althorchxr@gmail.com>
2025-02-18 17:19:47 +0800

    vue3

---
+++ MyVueTest/src/components/componentsTest02.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { inject } from 'vue';
+const msg = inject('item');
+
+const changeStr = inject('itemB');
+
+const changeVal = ()=>{
+  changeStr();
+}
+</script>
+<template>
+  <div class="son">
+    <h2>孙子组件</h2>
+    <h3>{{ msg }}</h3>
+    <button @click="changeVal">改变祖先组件的值</button>
+  </div>
+</template>
+
+<style scoped>
+.son {
+  padding: 51px;
+  border: 1px solid pink;
+}
+</style>
---
+++ MyVueTest/src/components/componentsTest03.vue
@@ -0,0 +1,17 @@
+<script setup>
+import componentsTest02 from './componentsTest02.vue';
+</script>
+
+<template>
+  <div class="father">
+    <h2>父组件</h2>
+    <componentsTest02></componentsTest02>
+  </div>
+</template>
+
+<style scoped>
+.father {
+  padding: 114px;
+  border: 1px solid greenyellow;
+}
+</style>
